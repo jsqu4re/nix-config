4dfa2f7685fb29a9e9d5b04c5629581a74a4b1a8
Johannes Jeising <johannes.jeising@gmail.com>
2024-05-11 12:46:02 +0200

    initial commit

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "A simple NixOS flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-23.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs: {
+    nixosConfigurations.tabula = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        # Import the previous configuration.nix we used,
+        # so the old configuration file still takes effect
+        ./configuration.nix
+
+        home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+
+            home-manager.users.jsqu4re = import ./home.nix;
+          }
+      ];
+    };
+  };
+}
