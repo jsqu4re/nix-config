9991034e64d880d8eb674dfac4e736e296edad80
Johannes Jeising <johannes.jeising@gmail.com>
2024-05-11 12:53:50 +0200

    testing ideas

--- home.nix
+++ home.nix
@@ -137,7 +137,6 @@
 
     # set some aliases, feel free to add more or remove some
     shellAliases = {
-      k = "kubectl";
       urldecode = "python3 -c 'import sys, urllib.parse as ul; print(ul.unquote_plus(sys.stdin.read()))'";
       urlencode = "python3 -c 'import sys, urllib.parse as ul; print(ul.quote_plus(sys.stdin.read()))'";
     };
@@ -154,5 +153,5 @@
   home.stateVersion = "23.11";
 
   # Let home Manager install and manage itself.
-  programs.home-manager.enable = true;
+  # programs.home-manager.enable = true;
 }
