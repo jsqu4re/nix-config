6d25f71d30ad4d371aecd3cefe258362256c3907
Johannes Jeising <johannes.jeising@gmail.com>
2024-06-14 23:40:26 +0200

    disable beta package

--- configuration.nix
+++ configuration.nix
@@ -54,7 +54,7 @@
     powerManagement.finegrained = false;
     open = false;
     nvidiaSettings = true;
-    package = config.boot.kernelPackages.nvidiaPackages.beta;
+    # package = config.boot.kernelPackages.nvidiaPackages.beta;
   };
 
   networking.networkmanager.enable = true;
