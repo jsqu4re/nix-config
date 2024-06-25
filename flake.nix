206c7374cd54d8f635c4b7608733359afe929a6a
Johannes Jeising <johannes.jeising@gmail.com>
2024-06-25 12:52:16 +0200

    use unstable branch

--- flake.nix
+++ flake.nix
@@ -2,8 +2,8 @@
   description = "A simple NixOS flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
-    # nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    # nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     home-manager = {
       url = "github:nix-community/home-manager/release-24.05";
       inputs.nixpkgs.follows = "nixpkgs";
