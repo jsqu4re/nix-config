e2cc377b3cac0447afacdd6edc12a2d5663acd23
Johannes Jeising <johannes.jeising@gmail.com>
2024-06-30 11:20:33 +0200

    disable anime wallpaper

--- home.nix
+++ home.nix
@@ -248,6 +248,10 @@
         ", XF86MonBrightnessDown, exec, brightnessctl set 5%-"
         ", XF86MonBrightnessUp, exec, brightnessctl set +5%"
       ];
+
+      misc = {
+        disable_hyprland_logo = true;
+      };
     };
   };
   
